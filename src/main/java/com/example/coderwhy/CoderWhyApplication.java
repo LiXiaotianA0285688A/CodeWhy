71b2cab1ab06a40861db9f5fcf5ddc326c5da455
root <admin@example.com>
2024-05-22 00:54:07 +0800

    first commit

---
+++ src/main/java/com/example/coderwhy/CoderWhyApplication.java
@@ -0,0 +1,13 @@
+package com.example.coderwhy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderWhyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoderWhyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/coderwhy/CoderWhyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coderwhy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoderWhyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
